852e40ea7ff2e49c567c3057866616842b6a5f32
merazuu <raju.karki@mavs.uta.edu>
2016-03-30 15:17:13 -0500

    JQuery cookie implemenation for saving username

--- popup.js
+++ popup.js
@@ -2,22 +2,46 @@ chrome.browserAction.setBadgeText({text: "5"}); // We have 10+ unread items.
 chrome.browserAction.setBadgeBackgroundColor({color: '#ffA400'});
 
 document.addEventListener('DOMContentLoaded', function() {
-    $(document).ready(function() {
-        $("#getResult").click(function() {
-            $.ajax({
-	                url: "https://jira2.cerner.com/rest/api/2/search?jql=assignee%3D" + $("#username").val() + "%20and%20status!%3DClosed",
-	                type: "GET",
-	                dataType: "json",
-	                async: true, 
-	                success: handleData,
-	                jsonp: false,
-	                error: function (response) {
-	                    $("#jira").text("error: " + response.responseText);
-	                }
-            });
+    $(document).ready(function () {
+       // $("#jira1").text($.cookie('cerner-username'));
+        if ($.cookie('cerner-username') == null) {
+            $("#username-input").show();
+            $("#jira-display").hide();
+        }
+        else {
+            $("#username-input").hide();
+            $("#jira-display").show();
+            PopulateJIRA();
+        }
+        $("#saveUserName").click(function () {
+            $("#username-input").hide();
+            $.cookie('cerner-username', $("#username").val(), { expires: 7, path: '/' });
+            PopulateJIRA();
+            $("#jira-display").show();
         })
+        $("#forget-user").click(function () {
+            $.removeCookie('cerner-username');
+            $("#username-input").show();
+            $("#jira-display").hide();
+        });
     });
-	function handleData(data) {
+
+    function PopulateJIRA() {
+        var username = $.cookie('cerner-username');
+        $.ajax({
+            url: "https://jira2.cerner.com/rest/api/2/search?jql=assignee%3D" + username + "%20and%20status!%3DClosed",
+            type: "GET",
+            dataType: "json",
+            async: true,
+            success: handleData,
+            jsonp: false,
+            error: function (response) {
+                $("#jira").text("error: " + response.responseText);
+            }
+        });
+    }
+
+    function handleData(data) {
 	    var cList = "";
 	    var link = "";
 	    $.each(data.issues, function (key, val) {
