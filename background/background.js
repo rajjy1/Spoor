04305489d8d5adcb0b46450da939a914362eb736
Dinesh Bajracharya <dinesh.bajracharya@cerner.com>
2016-03-31 11:16:32 -0500

    Added files for getting git reviews in background.

--- background/background.js
+++ background/background.js
@@ -1,40 +1,78 @@
-SPOOR_APP = {
-    id    : 1007,
 
-};
+var oldChromeVersion = !chrome.runtime;
+var delay = 5;
 
-SPOOR_API = {
-    token   : null,
-    user_id : null,
-
-    getJiraInfo          : 'https://url_for_jira/',
-    getCrucicbleInfo : 'https://url_for_crucible/'
-};
-
-
-
-function getNotifications() {
-    console.log('getNotifications()');
-
+if (oldChromeVersion) {
+  onInit();
+} else {
+  chrome.runtime.onInstalled.addListener(onInit);
+  chrome.alarms.onAlarm.addListener(onAlarm);
 }
 
-function setBadgeCount(count) {
-    console.log('setBadgeCount');
-    if (count === '0') {
-        count = '';
+function onAlarm(alarm) {
+  console.log('Got alarm', alarm);
+  startRequest();
+}
+
+function onInit() {
+  console.log('onInit');
+  startRequest();
+}
+
+function scheduleRequest() {
+  console.log('Scheduling for: ' + delay);
+
+  console.log('Creating alarm');
+    // Use a repeating alarm so that it fires again if there was a problem
+    // setting the next alarm.
+  chrome.alarms.create('refresh', {periodInMinutes: delay});
+}
+
+function startRequest(params) {
+  fetchGit();
+  scheduleRequest();
+}
+
+function fetchGit() {
+  console.log('FetchGit');
+  jQuery.ajax({
+    type : 'GET',
+    url : 'https://github.cerner.com/api/v3/users/DB029476/received_events?per_page=100',
+    dataType : 'json',
+    contentType : 'application/json',
+    async : false,    
+    success: function(response) {
+        console.log(response);
+      var pullrequestCount=0;
+
+      var pullRequestList=[];
+      if ( jQuery.isArray(response) ) {
+      
+      $.each(response, function(i, event) {
+       var eventType=event.type;
+       if(eventType=="PullRequestEvent"){
+          var payload=event.payload;
+          if(payload.action=="opened"){
+            var pullrequest=payload.pull_request;
+            pullRequestList.push(pullrequest.html_url);
+            pullrequestCount++;
+          }
+        }
+      });
+      } else {
+        // alert('Not array');
+      }
+      $("#githubCount").html(pullrequestCount);
+      chrome.browserAction.setBadgeText({text: ""+pullrequestCount}); 
+      var pullRequestElement="<ul>";
+      $.each(pullRequestList, function(i, pullRequest) {
+        pullRequestElement+="<li>"+"<a href="+pullRequest+">"+pullRequest+"</a>"+"</li>";
+      });
+      pullRequestElement+="</ul>";
+      $("#githubRequests").html(pullRequestElement);
+      },error: function(obj,error,errormsg){
+      console.log('Error');
+      
     }
-
-    chrome.browserAction.setBadgeText({
-        text : count
     });
 }
-
-chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
-    sendResponse({
-        messages : 'a new message has arrived',
-        userId   : 'user_id'
-    });
-});
-
-//ready, set, go!
-init();
